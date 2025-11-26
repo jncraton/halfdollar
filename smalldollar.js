c2dea1728bf0381305d210eb48726f1bb76d49b4
jncraton <jncraton@gmail.com>
2025-11-26 13:46:19 -0500

    Build proxy around nodelist

--- smalldollar.js
+++ smalldollar.js
@@ -1,1 +1,18 @@
-const $ = q => document.querySelectorAll(q)
+const proxy = {
+  get(target, prop) {
+    if (!(prop in target)) {
+      console.warn(`Attempted to read undefined property "${prop}"`)
+      return undefined
+    }
+    return target[prop]
+  },
+  set(target, prop, value, receiver) {
+    if (prop in target) {
+      return Reflect.set(target, prop, value, receiver)
+    }
+    console.warn(`Attempted to set undefined property "${prop}" to`, value)
+    return true
+  }
+}
+
+const $ = q => new Proxy(document.querySelectorAll(q), proxy)
