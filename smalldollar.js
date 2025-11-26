93af868af541ef0702b575cda7be826c44eac3de
jncraton <jncraton@gmail.com>
2025-11-26 16:01:14 -0500

    Add comments

--- smalldollar.js
+++ smalldollar.js
@@ -1,6 +1,27 @@
 const $ = q =>
   new Proxy(document.querySelectorAll(q), {
+    /*
+     * Intercept calls to the NodeList returned by querySelectorAll
+     * using a Proxy:
+     *
+     * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
+     *
+     * `set` is simple
+     *
+     * We exchange the ability to modify the NodeList
+     * and simply pass any modification on to every child element
+     */
     set: (target, prop, value) => target.map(el => (el[prop] = value)),
+    /*
+     * `get` is more complicated
+     *
+     * We may want access to NodeList properties (such as forEach), but
+     * we would also like to be able to call functions on individual
+     * elements (such as addEventListener).
+     *
+     * In addition to functions, we'd like to be able to access
+     * properties (such as textContent).
+     */
     get: (target, prop) => {
       if (prop in target) {
         return target[prop]
