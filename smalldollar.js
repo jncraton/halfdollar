0182765808897f68e08132748614634e2c4ccc30
jncraton <jncraton@gmail.com>
2025-11-26 13:58:11 -0500

    Prefer early return

--- smalldollar.js
+++ smalldollar.js
@@ -1,11 +1,12 @@
 const $ = q => new Proxy(document.querySelectorAll(q), {
   get(target, prop) {
-    if (!(prop in target)) {
-      return (...args) => target.forEach(el => {
-        el[prop](...args)
-      })
+    if (prop in target) {
+      return target[prop]
     }
-    return target[prop]
+
+    return (...args) => target.forEach(el => {
+      el[prop](...args)
+    })
   },
   set(target, prop, value, receiver) {
     if (prop in target) {
